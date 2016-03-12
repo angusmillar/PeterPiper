24d62d82b864e2bb07d5d70d4b8d4e0687850bb8
Angus Millar <angusmillar@iinet.net.au>
2016-03-12 13:46:59 +1000

    Removed all the Nehta CDA work as it was not core to this project

--- Glib/Hl7/Nehta/CDA/DataType/QuantityRange.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Glib.Hl7.Nehta.CDA.DataType
-{
-  public class QuantityRange<T> : IVL<T> where T : Quantity {}
-}
